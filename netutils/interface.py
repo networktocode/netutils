b7ab272ac618bab5ca5ac37414b7ec6abd830860
Adam Byczkowski <adam.byczkowski@networktocode.com>
2021-05-11 10:18:31 -0500

    Added ability to create interface lists

--- netutils/interface.py
+++ netutils/interface.py
@@ -64,6 +64,70 @@ def interface_range_expansion(interface_pattern):
     return expanded_interfaces
 
 
+def interface_tail_addition(interface_number):
+    """Takes the tail end of the interface and sums it up to use for sorting a list of interfaces.
+
+    Args:
+        interface_number (str): Interface number I.E: 1/0/1, 1/0, 0/1
+
+    Returns:
+        int: Sum of number in interface tail with each one getting a priority of magnitude 10 from right to left.
+    """
+    priority_values = []
+    factor_value = 1
+    split_post_sub = []
+
+    split_pre_sub = split_interface(interface_number)[1].split("/")
+    for item in split_pre_sub:
+        if "." in item:
+            split_post_sub.append(item.split(".")[0])
+        else:
+            split_post_sub.append(item)
+
+    values = [int(i) for i in split_post_sub]
+    for i in reversed(values):
+        i = i * factor_value
+        factor_value = factor_value * 10
+        priority_values.insert(0, i)
+    return sum(priority_values)
+
+
+def list_alphabetical(interfaces, reverse=False):
+    """Takes list of interfaces and ensures they are in alphabetical order.
+
+    Args:
+        interfaces (list): List of interfaces
+        reverse (bool): Determines if list alphabetical list should be reversed. Defaults to False.
+
+    Returns:
+        list: Interfaces listed in alphabetical order.
+    """
+    separate_interfaces_list = []
+    contained_interfaces = set()
+
+    for interface in interfaces:
+        interface_name = split_interface(interface)[0]
+        contained_interfaces.add(interface_name)
+
+    for interface_name in contained_interfaces:
+        new_list = [x for x in interfaces if split_interface(x)[0] == interface_name]
+        if new_list:
+            separate_interfaces_list.append(new_list)
+
+    for separate_list in separate_interfaces_list:
+        separate_list.sort(key=interface_tail_addition)
+
+    if reverse:
+        sorted_separate_list = reversed(sorted(separate_interfaces_list, key=lambda x: x[0]))
+    else:
+        sorted_separate_list = sorted(separate_interfaces_list, key=lambda x: x[0])
+
+    return list(itertools.chain.from_iterable(sorted_separate_list))
+
+
+INTERFACE_LIST_ORDERING_OPTIONS = {"alphabetical": list_alphabetical}
+
+
 def split_interface(interface):
     """Split an interface name based on first digit, slash, or space match.
 
@@ -127,6 +191,68 @@ def canonical_interface_name(interface, addl_name_map=None, verify=False):
     return interface
 
 
+def canonical_interface_name_list(interfaces, addl_name_map=None, verify=False, order=None, reverse=None):
+    """Function to return a list of interface's canonical name (fully expanded name).
+
+    Use of explicit matches used to indicate a clear understanding on any potential
+    match. Regex and other looser matching methods were not implmented to avoid false
+    positive matches. As an example, it would make sense to do "[P|p][O|o]" which would
+    incorrectly match PO = POS and Po = Port-channel, leading to a false positive, not
+    easily troubleshot, found, or known.
+
+    Args:
+        interfaces (list): List of interfaces you are attempting to expand.
+        addl_name_map (dict, optional): A dict containing key/value pairs that updates the base mapping. Used if an OS has specific differences. e.g. {"Po": "PortChannel"} vs {"Po": "Port-Channel"}. Defaults to None.
+        verify (bool, optional): Whether or not to verify the interface matches a known interface standard. Defaults to False.
+        order (str, optional): Determines what order the list of interfaces should be returned in. Defaults to None.
+        reverse (bool, optional): Specify if the order of the list should be reversed when setting an order. Defaults to None.
+
+    Returns:
+        list: List of the interfaces in their long form.
+
+    Raises:
+        ValueError: Raised if any interface name in list cannot be converted to its long form and verify parameter is set to true.
+
+    Example:
+        >>> from net_utility.interface import canonical_interface_name_list
+        >>> canonical_interface_name_list(["Gi1/0/1", "Gi1/0/2", "Eth1"])
+        ['GigabitEthernet1/0/1', 'GigabitEthernet1/0/2', 'Ethernet1']
+        >>> canonical_interface_name_list(["Gi1/0/1", "Po40", "Lo10"])
+        ['GigabitEthernet1/0/1', 'Port-channel40', 'Loopback10']
+        >>>
+    """
+    name_map = {}
+    name_map.update(BASE_INTERFACES)
+    canonical_interface_list = []
+    no_match_list = []
+
+    if reverse and not order:
+        raise ValueError("Order must be set to use reverse.")
+
+    if order and order not in INTERFACE_LIST_ORDERING_OPTIONS.keys():
+        raise ValueError(f"{order} is not one of the supported orderings")
+
+    if isinstance(addl_name_map, dict):
+        name_map.update(addl_name_map)
+
+    for interface in interfaces:
+        interface_type, interface_number = split_interface(interface)
+        if name_map.get(interface_type):
+            long_int = name_map.get(interface_type)
+            canonical_interface_list.append(long_int + str(interface_number))
+        else:
+            canonical_interface_list.append(interface)
+            no_match_list.append(interface)
+
+    if verify:
+        no_match_string = ", ".join(no_match_list)
+        raise ValueError(f"Verify interface on and no match found for {no_match_string}")
+
+    if order:
+        canonical_interface_list = INTERFACE_LIST_ORDERING_OPTIONS.get(order)(canonical_interface_list, reverse)
+    return canonical_interface_list
+
+
 def abbreviated_interface_name(interface, addl_name_map=None, addl_reverse_map=None, verify=False):
     """Function to return an abbreviated representation of the interface name.
 
@@ -178,6 +304,7 @@ def abbreviated_interface_name(interface, addl_name_map=None, addl_reverse_map=N
     return interface
 
 
+<<<<<<< HEAD
 @total_ordering
 class CharacterClass(ABC):
     """CharacterClass embodies the state needed to sort interfaces."""
@@ -352,3 +479,74 @@ def sort_interface_list(interfaces: t.List[str]) -> t.List[str]:
     for ifname in interfaces:
         _insert_nodes(root, _split_interface_tuple(ifname))
     return list(iter_tree(root, []))
+=======
+def abbreviated_interface_name_list(  # pylint: disable=R0913, R0914
+    interfaces, addl_name_map=None, addl_reverse_map=None, verify=False, order=None, reverse=None
+):
+    """Function to return a list of interface's abbreviated name.
+
+    Args:
+        interfaces (list): List of interface names you are attempting to abbreviate.
+        addl_name_map (dict, optional): A dict containing key/value pairs that updates the base mapping. Used if an OS has specific differences. e.g. {"Po": "PortChannel"} vs {"Po": "Port-Channel"}. Defaults to None.
+        addl_reverse_map (dict, optional): A dict containing key/value pairs that updates the abbreviated mapping. Defaults to None.
+        verify (bool, optional): Whether or not to verify the interface matches a known interface standard. Defaults to False.
+        order (str, optional): Determines what order the list of interfaces should be returned in. Defaults to None.
+        reverse (bool, optional): Specify if the order of the list should be reversed when setting an order. Defaults to None.
+
+    Returns:
+        list: List of the interfaces in their abbreviated form.
+
+    Raises:
+        ValueError: Raised if any interface name in list cannot be converted to its abbreviated form and verify parameter is set to true.
+
+    Example:
+        >>> from netutils.interface import abbreviated_interface_name_list
+        >>> abbreviated_interface_name_list(["GigabitEthernet1/0/1", "GigabitEthernet1/0/2", "Ethernet1"])
+        ['Gi1/0/1', 'Gi1/0/2', 'Et1']
+        >>> abbreviated_interface_name_list(['GigabitEthernet1/0/1', 'Port-channel40', 'Loopback10'])
+        ['Gi1/0/1', 'Po40', 'Lo10']
+        >>>
+    """
+    name_map = {}
+    name_map.update(BASE_INTERFACES)
+    abbreviated_interface_list = []
+    no_match_list = []
+
+    if reverse and not order:
+        raise ValueError("Order must be set to use reverse.")
+
+    if order and order not in INTERFACE_LIST_ORDERING_OPTIONS.keys():
+        raise ValueError(f"{order} is not one of the supported orderings")
+
+    if isinstance(addl_name_map, dict):
+        name_map.update(addl_name_map)
+
+    rev_name_map = {}
+    rev_name_map.update(REVERSE_MAPPING)
+
+    if isinstance(addl_reverse_map, dict):
+        rev_name_map.update(addl_reverse_map)
+
+    for interface in interfaces:
+        interface_type, interface_number = split_interface(interface)
+        # Try to ensure canonical type.
+        if name_map.get(interface_type):
+            canonical_type = name_map.get(interface_type)
+        else:
+            canonical_type = interface_type
+
+        try:
+            abbreviated_name = rev_name_map[canonical_type] + str(interface_number)
+            abbreviated_interface_list.append(abbreviated_name)
+        except KeyError:
+            abbreviated_interface_list.append(interface)
+            no_match_list.append(interface)
+
+    if verify:
+        no_match_string = ", ".join(no_match_list)
+        raise ValueError(f"Verify interface on and no match found for {no_match_string}")
+
+    if order:
+        abbreviated_interface_list = INTERFACE_LIST_ORDERING_OPTIONS.get(order)(abbreviated_interface_list, reverse)
+    return abbreviated_interface_list
+>>>>>>> Added ability to create interface lists
--- tests/unit/test_interface.py
+++ tests/unit/test_interface.py
@@ -25,6 +25,87 @@ CANONICAL_INTERFACE_NAME = [
     {"sent": {"interface": "Noninterface1/0/1"}, "received": "Noninterface1/0/1"},
 ]
 
+CANONICAL_INTERFACE_NAME_LIST = [
+    {
+        "sent": {"interfaces": ["GigabitEthernet1/0/1", "GigabitEthernet1/0/2"]},
+        "received": ["GigabitEthernet1/0/1", "GigabitEthernet1/0/2"],
+    },
+    {
+        "sent": {"interfaces": ["Gi1/0/1", "Po40", "Lo10"]},
+        "received": ["GigabitEthernet1/0/1", "Port-channel40", "Loopback10"],
+    },
+    {
+        "sent": {"interfaces": ["Gi1/0/1", "Gi1/0/3", "Gi1/0/2", "Po40", "Po160", "Lo10"], "order": "alphabetical"},
+        "received": [
+            "GigabitEthernet1/0/1",
+            "GigabitEthernet1/0/2",
+            "GigabitEthernet1/0/3",
+            "Loopback10",
+            "Port-channel40",
+            "Port-channel160",
+        ],
+    },
+    {
+        "sent": {
+            "interfaces": ["Gi1/0/1", "Gi1/0/3", "Gi1/0/3.100", "Gi1/0/2", "Gi1/0/2.50", "Po40", "Po160", "Lo10"],
+            "order": "alphabetical",
+        },
+        "received": [
+            "GigabitEthernet1/0/1",
+            "GigabitEthernet1/0/2",
+            "GigabitEthernet1/0/2.50",
+            "GigabitEthernet1/0/3",
+            "GigabitEthernet1/0/3.100",
+            "Loopback10",
+            "Port-channel40",
+            "Port-channel160",
+        ],
+    },
+    {
+        "sent": {
+            "interfaces": ["Gi1/0/1", "Gi1/0/3", "Gi1/0/2", "Po40", "Po160", "Lo10"],
+            "order": "alphabetical",
+            "reverse": True,
+        },
+        "received": [
+            "Port-channel40",
+            "Port-channel160",
+            "Loopback10",
+            "GigabitEthernet1/0/1",
+            "GigabitEthernet1/0/2",
+            "GigabitEthernet1/0/3",
+        ],
+    },
+    {
+        "sent": {
+            "interfaces": ["Gi1/0/1", "Gi1/0/3", "Gi1/0/3.100", "Gi1/0/2", "Gi1/0/2.50", "Po40", "Po160", "Lo10"],
+            "order": "alphabetical",
+            "reverse": True,
+        },
+        "received": [
+            "Port-channel40",
+            "Port-channel160",
+            "Loopback10",
+            "GigabitEthernet1/0/1",
+            "GigabitEthernet1/0/2",
+            "GigabitEthernet1/0/2.50",
+            "GigabitEthernet1/0/3",
+            "GigabitEthernet1/0/3.100",
+        ],
+    },
+    {
+        "sent": {
+            "interfaces": ["SuperFastEth 1/0/1", "SuperFastEth 1/0/2"],
+            "addl_name_map": {"SuperFastEth": "SuperFastEthernet"},
+        },
+        "received": ["SuperFastEthernet1/0/1", "SuperFastEthernet1/0/2"],
+    },
+    {
+        "sent": {"interfaces": ["Noninterface1/0/1", "Noninterface1/0/2"]},
+        "received": ["Noninterface1/0/1", "Noninterface1/0/2"],
+    },
+]
+
 ABBREVIATED_INTERFACE_NAME = [
     {
         "sent": {"interface": "GigabitEthernet1/0/1"},
@@ -165,7 +246,6 @@ INTERFACE_SORT = [
             "Gi0/0/1",
             "Gi0/0/2",
             "Gi0/1",
-            "Gi1/0/1",
             "Gi1/0/2",
             "Gi1/0/2.50",
             "Gi1/0/3",
@@ -198,6 +278,184 @@ BAD_INTERFACE_NAMES = [
     "Te42/55.32/77@99",
 ]
 
+ABBREVIATED_INTERFACE_NAME_LIST = [
+    {
+        "sent": {"interfaces": ["Gi1/0/1", "Gi1/0/2"]},
+        "received": ["Gi1/0/1", "Gi1/0/2"],
+    },
+    {
+        "sent": {"interfaces": ["GigabitEthernet1/0/1", "Po40", "Loopback10"]},
+        "received": ["Gi1/0/1", "Po40", "Lo10"],
+    },
+    {
+        "sent": {
+            "interfaces": [
+                "GigabitEthernet1/0/1",
+                "GigabitEthernet1/0/3",
+                "GigabitEthernet1/0/2",
+                "Loopback10",
+                "Port-channel160",
+                "Port-channel40",
+            ],
+            "order": "alphabetical",
+        },
+        "received": ["Gi1/0/1", "Gi1/0/2", "Gi1/0/3", "Lo10", "Po40", "Po160"],
+    },
+    {
+        "sent": {
+            "interfaces": [
+                "GigabitEthernet1/0/1",
+                "GigabitEthernet1/0/3",
+                "GigabitEthernet1/0/3.100",
+                "GigabitEthernet1/0/2",
+                "GigabitEthernet1/0/2.50",
+                "Loopback10",
+                "Port-channel160",
+                "Port-channel40",
+            ],
+            "order": "alphabetical",
+        },
+        "received": ["Gi1/0/1", "Gi1/0/2", "Gi1/0/2.50", "Gi1/0/3", "Gi1/0/3.100", "Lo10", "Po40", "Po160"],
+    },
+    {
+        "sent": {
+            "interfaces": [
+                "GigabitEthernet1/0/1",
+                "GigabitEthernet1/0/3",
+                "GigabitEthernet1/0/2",
+                "Loopback10",
+                "Port-channel160",
+                "Port-channel40",
+            ],
+            "order": "alphabetical",
+            "reverse": True,
+        },
+        "received": ["Po40", "Po160", "Lo10", "Gi1/0/1", "Gi1/0/2", "Gi1/0/3"],
+    },
+    {
+        "sent": {
+            "interfaces": [
+                "GigabitEthernet1/0/1",
+                "GigabitEthernet1/0/3",
+                "GigabitEthernet1/0/3.100",
+                "GigabitEthernet1/0/2",
+                "GigabitEthernet1/0/2.50",
+                "Loopback10",
+                "Port-channel160",
+                "Port-channel40",
+            ],
+            "order": "alphabetical",
+            "reverse": True,
+        },
+        "received": [
+            "Po40",
+            "Po160",
+            "Lo10",
+            "Gi1/0/1",
+            "Gi1/0/2",
+            "Gi1/0/2.50",
+            "Gi1/0/3",
+            "Gi1/0/3.100",
+        ],
+    },
+    {
+        "sent": {
+            "interfaces": ["SuperFastEthernet1/0/1", "SuperFastEthernet1/0/2"],
+            "addl_name_map": {"SupFaEth": "SuperFastEthernet"},
+            "addl_reverse_map": {"SuperFastEthernet": "SupFaEth"},
+        },
+        "received": ["SupFaEth1/0/1", "SupFaEth1/0/2"],
+    },
+    {
+        "sent": {"interfaces": ["Noninterface1/0/1", "Noninterface1/0/2"]},
+        "received": ["Noninterface1/0/1", "Noninterface1/0/2"],
+    },
+]
+
+ORDERED_INTERFACE_NAME_LIST = [
+    {
+        "sent": {
+            "interfaces": [
+                "FastEthernet0/0",
+                "GigabitEthernet0/0",
+                "FortyGigabitEthernet0/0",
+                "HundredGigabitEthernet0/0",
+                "FourHundredGigabitEthernet0/0",
+                "Loopback0/0",
+                "Ethernet0/0",
+            ]
+        },
+        "received": [
+            "Ethernet0/0",
+            "FastEthernet0/0",
+            "FortyGigabitEthernet0/0",
+            "FourHundredGigabitEthernet0/0",
+            "GigabitEthernet0/0",
+            "HundredGigabitEthernet0/0",
+            "Loopback0/0",
+        ],
+    },
+    {
+        "sent": {
+            "interfaces": [
+                "FastEthernet1/1",
+                "FastEthernet1/3",
+                "FastEthernet1/2",
+                "FastEthernet0/1",
+                "FastEthernet0/3",
+                "FastEthernet0/2",
+                "HundredGigabitEthernet1/0/0",
+                "HundredGigabitEthernet1/0/2",
+                "HundredGigabitEthernet1/0/1",
+                "Ethernet0",
+                "Ethernet2",
+                "Ethernet1",
+            ]
+        },
+        "received": [
+            "Ethernet0",
+            "Ethernet1",
+            "Ethernet2",
+            "FastEthernet0/1",
+            "FastEthernet0/2",
+            "FastEthernet0/3",
+            "FastEthernet1/1",
+            "FastEthernet1/2",
+            "FastEthernet1/3",
+            "HundredGigabitEthernet1/0/0",
+            "HundredGigabitEthernet1/0/1",
+            "HundredGigabitEthernet1/0/2",
+        ],
+    },
+    {
+        "sent": {
+            "interfaces": [
+                "FastEthernet0/1",
+                "FastEthernet0/3",
+                "FastEthernet0/2",
+                "HundredGigabitEthernet1/0/0",
+                "HundredGigabitEthernet1/0/2",
+                "HundredGigabitEthernet1/0/1",
+                "Ethernet0",
+                "Ethernet2",
+                "Ethernet1",
+            ],
+            "reverse": True,
+        },
+        "received": [
+            "HundredGigabitEthernet1/0/0",
+            "HundredGigabitEthernet1/0/1",
+            "HundredGigabitEthernet1/0/2",
+            "FastEthernet0/1",
+            "FastEthernet0/2",
+            "FastEthernet0/3",
+            "Ethernet0",
+            "Ethernet1",
+            "Ethernet2",
+        ],
+    },
+]
+
 
 @pytest.mark.parametrize("data", SPLIT_INTERFACE)
 def test_split_interface(data):
@@ -213,6 +471,34 @@ def test_canonical_interface_name_failure():
     with pytest.raises(ValueError, match=r"Verify interface on and no match found for*"):
         data = {"interface": "SuperFastEth 1/0/1", "verify": True}
         interface.canonical_interface_name(**data)
+
+
+@pytest.mark.parametrize("data", CANONICAL_INTERFACE_NAME_LIST)
+def test_canonical_interface_name_list(data):
+    assert interface.canonical_interface_name_list(**data["sent"]) == data["received"]
+
+
+def test_canonical_interface_name_list_failure():
+    with pytest.raises(ValueError, match=r"Verify interface on and no match found for [^,]*, [^ ]*"):
+        data = {"interfaces": ["SuperFastEth1/0/1", "SuperFastEth1/0/2"], "verify": True}
+        interface.canonical_interface_name_list(**data)
+
+
+def test_canonical_interface_name_list_order_failure():
+    with pytest.raises(ValueError, match=r"weight is not one of the supported orderings"):
+        data = {"interfaces": ["SuperFastEth1/0/1", "SuperFastEth1/0/2"], "order": "weight"}
+        interface.canonical_interface_name_list(**data)
+
+
+@pytest.mark.parametrize("data", ABBREVIATED_INTERFACE_NAME_LIST)
+def test_abbreviated_interface_name_list(data):
+    assert interface.abbreviated_interface_name_list(**data["sent"]) == data["received"]
+
+
+def test_abbreviated_interface_name_list_failure():
+    with pytest.raises(ValueError, match=r"Verify interface on and no match found for [^,]*, [^ ]*"):
+        data = {"interfaces": ["SuperFastEth1/0/1", "SuperFastEth1/0/2"], "verify": True}
+        interface.canonical_interface_name_list(**data)
 
 
 @pytest.mark.parametrize("data", ABBREVIATED_INTERFACE_NAME)
@@ -251,3 +537,13 @@ def test_interface_sort_prune(data):
 def test_interface_sort_order(data):
     """Assert that the tree iterates in canonical order."""
     assert interface.sort_interface_list(data["sent"]) == data["received"]
+
+def test_abbreviated_interface_name_order_failure():
+    with pytest.raises(ValueError, match=r"weight is not one of the supported orderings"):
+        data = {"interfaces": "SuperFastEth 1/0/1", "order": "weight"}
+        interface.abbreviated_interface_name_list(**data)
+
+
+@pytest.mark.parametrize("data", ORDERED_INTERFACE_NAME_LIST)
+def test_list_alphabetical(data):
+    assert interface.list_alphabetical(**data["sent"]) == data["received"]
