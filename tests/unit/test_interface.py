b6ac6f2ac04c3890bcc2c51ed1b7b5eede03727d
Daniel <djustice@wificidr.net>
2021-07-28 10:17:46 -0500

    Add interface sorting support.

--- tests/unit/test_interface.py
+++ tests/unit/test_interface.py
@@ -98,6 +98,106 @@ INTERFACE_EXPANSION = [
     {"sent": "[1,2]/0/[1-2]", "received": ["1/0/1", "1/0/2", "2/0/1", "2/0/2"]},
 ]
 
+INTERFACE_SORT = [
+    {"sent": [], "received": []},
+    {"sent": ["Fa0/0/1.200", "Fa0/0/1.200", "Fa0/0/1.200"], "received": ["Fa0/0/1.200"]},
+    {
+        "sent": [
+            "Fa0/0/2.200",
+            "Fa1/0/1.100",
+            "Fa1/0.100",
+            "Gi1/1",
+            "Gi0/1",
+            "Gi0/0/1",
+            "Gi1/0/2",
+            "Vlan42",
+            "Te/42",
+            "loopback99",
+            "Port-channel75",
+            "Gi1/0/1",
+            "Fa0/0/2",
+            "Fa0/0/1",
+            "Fa0/0/1.101",
+            "Fa0/0/1.100",
+            "Fa0/0/1.10",
+            "Fa0/0/1.500",
+            "Fa0/0/1.999",
+            "Gi/42",
+            "Loopback101",
+            "Loopback102",
+            "Loopback98",
+            "Loopback99",
+            "Port-channel160",
+            "Port-channel40",
+            "Fa0/0/1.4",
+            "Fa42",
+            "Gi1/0/3",
+            "Gi1/0/3.100",
+            "Fa1",
+            "Gi1/0/2",
+            "Gi1/0/2.50",
+            "Zf.42",
+            "Fa1.42",
+            "Po40",
+            "Po160",
+            "Lo10",
+            "Fa1/42",
+            "Gi0/0/2",
+            "Gi1/0/1",
+            "Te1/0/1",
+        ],
+        "received": [
+            "Fa0/0/1",
+            "Fa0/0/1.4",
+            "Fa0/0/1.10",
+            "Fa0/0/1.100",
+            "Fa0/0/1.101",
+            "Fa0/0/1.500",
+            "Fa0/0/1.999",
+            "Fa0/0/2",
+            "Fa0/0/2.200",
+            "Fa1",
+            "Fa1.42",
+            "Fa1/0.100",
+            "Fa1/0/1.100",
+            "Fa1/42",
+            "Fa42",
+            "Gi0/0/1",
+            "Gi0/0/2",
+            "Gi0/1",
+            "Gi1/0/1",
+            "Gi1/0/2",
+            "Gi1/0/2.50",
+            "Gi1/0/3",
+            "Gi1/0/3.100",
+            "Gi1/1",
+            "Gi/42",
+            "Lo10",
+            "Loopback98",
+            "Loopback99",
+            "Loopback101",
+            "Loopback102",
+            "Po40",
+            "Po160",
+            "Port-channel40",
+            "Port-channel75",
+            "Port-channel160",
+            "Te1/0/1",
+            "Te/42",
+            "Vlan42",
+            "Zf.42",
+            "loopback99",
+        ],
+    },
+]
+
+BAD_INTERFACE_NAMES = [
+    "fa?",
+    "Gi1^",
+    "Fa0/0*/42",
+    "Te42/55.32/77@99",
+]
+
 
 @pytest.mark.parametrize("data", SPLIT_INTERFACE)
 def test_split_interface(data):
@@ -129,3 +229,25 @@ def test_abbreviated_interface_name_failure():
 @pytest.mark.parametrize("data", INTERFACE_EXPANSION)
 def test_interface_range_expansion(data):
     assert interface.interface_range_expansion(data["sent"]) == data["received"]
+
+
+@pytest.mark.parametrize("data", BAD_INTERFACE_NAMES)
+def test_split_interface_tuple_fails(data):
+    with pytest.raises(ValueError):
+        interface.split_interface_tuple(data)
+
+
+def test_interface_sort_empty():
+    assert interface.sort_interface_list([]) == []
+
+
+@pytest.mark.parametrize("data", INTERFACE_SORT)
+def test_interface_sort_prune(data):
+    """Assert that duplicate nodes are pruned from the tree."""
+    assert set(interface.sort_interface_list(data["sent"])) == set(data["received"])
+
+
+@pytest.mark.parametrize("data", INTERFACE_SORT)
+def test_interface_sort_order(data):
+    """Assert that the tree iterates in canonical order."""
+    assert interface.sort_interface_list(data["sent"]) == data["received"]
