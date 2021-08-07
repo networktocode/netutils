be30bccbea9779779e69666e9b0ef9097d470ff3
jtdub <amascuba@gmail.com>
2021-08-06 21:16:54 -0500

    add tcp/udp port mapper

--- tests/unit/test_protocol_mapper.py
+++ tests/unit/test_protocol_mapper.py
@@ -1,7 +1,7 @@
 """Test for the lib_mapper definitions."""
 import pytest
 
-from netutils.protocol_mapper import PROTO_NAME_TO_NUM, PROTO_NUM_TO_NAME
+from netutils.protocol_mapper import PROTO_NAME_TO_NUM, PROTO_NUM_TO_NAME, TCP_UDP_NAME_TO_NUM, TCP_UDP_NUM_TO_NAME
 
 
 def test_proto_name_to_num():
@@ -24,3 +24,13 @@ def test_name_not_exist_num_to_nam():
     """Test not existed key raises error."""
     with pytest.raises(KeyError):
         PROTO_NUM_TO_NAME[257]  # pylint: disable=pointless-statement
+
+
+def test_tcp_name_to_num():
+    """Simple test of TCP protocol to number mapper."""
+    assert TCP_UDP_NAME_TO_NUM["SSH"] == 22
+
+
+def test_tcp_to_num_name():
+    """Simple test of TCP protocol to name mapper."""
+    assert TCP_UDP_NUM_TO_NAME[22] == "SSH"
