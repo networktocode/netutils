04f25060b50d5468b976a7a10c3eac429981d535
pke11y <paddyk@gmail.com>
2021-12-01 00:00:45 +0000

    Normalise banner demiliter for IOS to ^C & support parsing delimiter ^ (#69)
    * Normailise banner demiliter for IOS to ^C & support parsing delimiter ^

--- tests/unit/mock/config/compliance/compliance/cisco_ios/ios_basic_feature.py
+++ tests/unit/mock/config/compliance/compliance/cisco_ios/ios_basic_feature.py
@@ -2,4 +2,5 @@ features = [
     {"name": "bgp", "ordered": True, "section": ["router bgp "]},
     {"name": "snmp", "ordered": True, "section": ["snmp-server "]},
     {"name": "ntp", "ordered": False, "section": ["ntp server "]},
+    {"name": "banner", "ordered": True, "section": ["banner "]},
 ]
