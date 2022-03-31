7287d6ec82515d8d88ed54732d77eb88661ef89e
Ken Celenza <ken@celenza.org>
2022-03-31 16:22:22 -0400

    Fix issue when backup or intended is empty (#107)

---
+++ tests/unit/mock/config/compliance/feature_compliance/ios_empty_src_received.py
@@ -0,0 +1,10 @@
+data = {
+    "actual": "",
+    "cannot_parse": True,
+    "compliant": False,
+    "extra": "",
+    "intended": "ntp server 10.10.10.10\n" "ntp server 10.10.10.12\n" "ntp server 10.10.10.11",
+    "missing": "ntp server 10.10.10.10\n" "ntp server 10.10.10.12\n" "ntp server 10.10.10.11",
+    "ordered_compliant": False,
+    "unordered_compliant": False,
+}
