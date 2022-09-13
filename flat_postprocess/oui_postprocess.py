f53fae879035eca928f88c02a3535e64d1fde0b5
Adam Byczkowski <adam.byczkowski@kochgs.com>
2022-09-13 14:00:55 -0500

    Changed ci time

--- flat_postprocess/oui_postprocess.py
+++ flat_postprocess/oui_postprocess.py
@@ -3,17 +3,17 @@ import sys
 import re
 import json
 
-hex_re = r"^(?P<hex>[A-Fa-f0-9]{6})\s+\(.*\)[^a-zA-Z0-9]+(?P<company>.*)$"
+HEX_RE = r"^(?P<hex>[A-Fa-f0-9]{6})\s+\(.*\)[^a-zA-Z0-9]+(?P<company>.*)$"
 OUI_MAPPINGS = {}
 
 if __name__ == "__main__":
     print(f"argv: {sys.argv}")
-    with open(sys.argv[1], "r") as oui_file:
+    with open(sys.argv[1], "r", encoding="utf-8") as oui_file:
         for line in oui_file:
-            if re.search(hex_re, line):
-                group_regex_values = re.search(hex_re, line).groupdict()
+            if re.search(HEX_RE, line):
+                group_regex_values = re.search(HEX_RE, line).groupdict()
                 if group_regex_values.get("hex") and group_regex_values.get("company"):
                     OUI_MAPPINGS.update({group_regex_values.get("hex").lower(): group_regex_values.get("company")})
 
-    with open(sys.argv[1], "w") as oui_mappings:
+    with open(sys.argv[1], "w", encoding="utf-8") as oui_mappings:
         json.dump(OUI_MAPPINGS, oui_mappings, indent=2)
