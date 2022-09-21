e295cf612a5dec54980d92614559b457c2c88bd6
Adam Byczkowski <adam.byczkowski@kochgs.com>
2022-09-21 09:09:16 -0500

    postprocess updates

--- flat_postprocess/oui_postprocess.py
+++ flat_postprocess/oui_postprocess.py
@@ -1,7 +1,6 @@
 """Python code used to postprocess Flat github action data related to OUI mappings."""
 import sys
 import re
-import typing
 
 HEX_RE = r"^(?P<hex>[A-Fa-f0-9]{6})\s+\(.*\)[^a-zA-Z0-9]+(?P<company>.*)$"
 OUI_MAPPINGS = {}
@@ -15,7 +14,8 @@ if __name__ == "__main__":
                     OUI_MAPPINGS.update({group_regex_values.get("hex").lower(): group_regex_values.get("company")})
 
     with open(sys.argv[1], "w", encoding="utf-8") as oui_mappings:
-        oui_mappings.write('"""Dictionary object to store OUI information."""\n\n')
+        oui_mappings.write('"""Dictionary object to store OUI information."""\n')
+        oui_mappings.write("import typing\n\n")
         oui_mappings.write("OUI_MAPPINGS: typing.Dict[str, str] = {\n")
         for mac, company in OUI_MAPPINGS.items():
             oui_mappings.write(f'    "{mac}": "{company}",\n')
