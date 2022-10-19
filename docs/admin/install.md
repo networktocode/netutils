a1bf8a01a1eca0d8addd206592bd443a652273b7
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2022-10-19 15:55:59 -0500

    Add Napalm Getter feature (#149)
    Add Napalm Getter feature

--- docs/admin/install.md
+++ docs/admin/install.md
@@ -6,8 +6,19 @@ Option 1: Install from PyPI.
 $ pip install netutils
 ```
 
-Option 2: Install from a GitHub branch, such as develop as shown below.
+Option 2: Install from PyPI with optional requirements.
+
+```bash
+$ pip install netutils[napalm]
+```
+
+Option 3: Install from a GitHub branch, such as develop as shown below.
 
 ```bash
 $ pip install git+https://github.com/networktocode/netutils.git@develop
 ```
+
+### Optional Dependencies
+One of the requirements of this library is to avoid having dependencies; however, including a few optional dependencies in an opt in fashion allows `netutils` to remain lean while offering some powerful addons.
+
+Installing the optional dependencies is only needed when the user needs access to the functions using the dependencies. As an example `get_napalm_getters()` which will provide a mapping of available NAPALM getters based on currently installed NAPALM libraries, if NAPALM is not installed the function simply raises an exception and warns the user that the library is not installed.
---
+++ tests/unit/test_lib_helpers_optionals.py
@@ -0,0 +1,59 @@
+"""Test for the lib_helpers definitions when optional packages are installed."""
+from unittest import mock
+
+import pytest
+from netutils.lib_helpers import get_napalm_getters
+
+
+def test_get_napalm_getters_napalm_installed_default():
+    pytest.importorskip("napalm")
+    with mock.patch("netutils.lib_helpers.get_network_driver"):
+        napalm_getters = get_napalm_getters()
+        assert napalm_getters == {
+            "asa": {},
+            "cisco_wlc_ssh": {},
+            "eos": {},
+            "fortios": {},
+            "huawei": {},
+            "ios": {},
+            "iosxr": {},
+            "junos": {},
+            "nxos": {},
+            "nxos_ssh": {},
+            "panos": {},
+            "sros": {},
+            "vyos": {},
+        }
+
+
+def test_get_napalm_getters_napalm_installed_nxos_keys():
+    pytest.importorskip("napalm")
+    napalm_getters = get_napalm_getters()
+    assert list(napalm_getters["nxos"].keys()) == [
+        "get_arp_table",
+        "get_bgp_config",
+        "get_bgp_neighbors",
+        "get_bgp_neighbors_detail",
+        "get_config",
+        "get_environment",
+        "get_facts",
+        "get_firewall_policies",
+        "get_interfaces",
+        "get_interfaces_counters",
+        "get_interfaces_ip",
+        "get_ipv6_neighbors_table",
+        "get_lldp_neighbors",
+        "get_lldp_neighbors_detail",
+        "get_mac_address_table",
+        "get_network_instances",
+        "get_ntp_peers",
+        "get_ntp_servers",
+        "get_ntp_stats",
+        "get_optics",
+        "get_probes_config",
+        "get_probes_results",
+        "get_route_to",
+        "get_snmp_information",
+        "get_users",
+        "get_vlans",
+    ]
