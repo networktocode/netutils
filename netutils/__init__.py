b2f680755aab4dac1297f5b2ea88d330c00a924f
itdependsnetworks <ken@celenza.org>
2022-10-27 16:12:33 -0400

    Ignore mypy on metadata 3.8 issues

--- netutils/__init__.py
+++ netutils/__init__.py
@@ -1,9 +1,9 @@
 """Initialization file for library."""
 
 try:
-    from importlib import metadata
+    from importlib import metadata  # type: ignore[attr-defined]
 except ImportError:
     # Python version < 3.8
-    import importlib_metadata as metadata
+    import importlib_metadata as metadata  # type: ignore[no-redef]
 
 __version__ = metadata.version(__name__)
