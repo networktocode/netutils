da5423db1c5bf9499b2a9470f19a30ea4dd6d880
Justin Drew <jdrew82@users.noreply.github.com>
2022-12-06 22:13:31 -0600

    test: ✅ Add remaining compliance tests for IOS-XR.

---
+++ tests/unit/mock/config/compliance/config_section_not_parsed/cisco_iosxr/iosxr_basic_feature.py
@@ -0,0 +1,5 @@
+features = [
+    {"name": "banner", "ordered": False, "section": ["banner"]},
+    {"name": "bgp", "ordered": False, "section": ["router bgp"]},
+    {"name": "ntp", "ordered": True, "section": ["ntp"]},
+]
