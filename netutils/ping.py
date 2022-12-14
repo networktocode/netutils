58bd9212747d9fa76802ef988796161bfee66f34
Andrew Bates <andrew.bates@networktocode.com>
2022-12-14 16:55:19 -0500

    fix: Fixes incorrect behavior when calling tcp_ping with timeout >= 3
    Fixes #175

--- netutils/ping.py
+++ netutils/ping.py
@@ -25,7 +25,10 @@ def tcp_ping(ip: str, port: int, timeout: int = 1) -> bool:  # pylint: disable=i
     sckt.settimeout(int(timeout))
     try:
         sckt.connect((ip, int(port)))  # pylint: disable=invalid-name
-        sckt.shutdown(int(timeout))
+        sckt.shutdown(socket.SHUT_RDWR)
         return True
-    except socket.error:
+    # We really only want to know if the TCP connection timed out
+    # or if there was an error resolving the DNS name, if anything
+    # else has happened the error should be raised.
+    except (socket.timeout, socket.gaierror):
         return False
--- tests/unit/test_ping.py
+++ tests/unit/test_ping.py
@@ -7,10 +7,11 @@ from netutils import ping
 ping_data = [
     {"sent": {"ip": "1.1.1.1", "port": 443}, "received": True},
     {"sent": {"ip": "nevergonnagiveyouup.pizza", "port": 443}, "received": False},
+    {"sent": {"ip": "127.254.254.254", "port": 443}, "received": False},
+    {"sent": {"ip": "1.1.1.1", "port": 443, "timeout": 3}, "received": True},
 ]
 
 
-@pytest.mark.skip(reason="Integration test not working in CI")
 @pytest.mark.parametrize("data", ping_data)
 def test_tcp_ping(data):
     assert ping.tcp_ping(**data["sent"]) == data["received"]
