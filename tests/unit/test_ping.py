c47f95f2b904ff191d7b817d63f6de022e725e93
Andrew Bates <andrew.bates@networktocode.com>
2022-12-15 11:20:14 -0500

    More appropriate test for exceptions

--- tests/unit/test_ping.py
+++ tests/unit/test_ping.py
@@ -7,7 +7,7 @@ from netutils import ping
 
 ping_data = [
     {"sent": {"ip": "1.1.1.1", "port": 443}, "received": True},
-    {"sent": {"ip": "nevergonnagiveyouup.pizza", "port": 443}, "received": False, "raises": socket.gaierror},
+    {"sent": {"ip": "nevergonnagiveyouup.pizza", "port": 443}, "received": None, "raises": socket.gaierror},
     {"sent": {"ip": "192.0.2.0", "port": 443}, "received": False},
     {"sent": {"ip": "1.1.1.1", "port": 443, "timeout": 3}, "received": True},
 ]
@@ -15,7 +15,10 @@ ping_data = [
 
 @pytest.mark.parametrize("data", ping_data)
 def test_tcp_ping(data):
+    raised = None
     try:
         assert ping.tcp_ping(**data["sent"]) == data["received"]
-    except data.get("raises", None):
-        pass
+    except Exception as ex:
+        raised = ex
+
+    assert isinstance(raised, data.get("raises", None.__class__))
