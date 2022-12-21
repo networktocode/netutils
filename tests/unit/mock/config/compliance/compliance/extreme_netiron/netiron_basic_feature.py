5983fb315650de0ddc4118fa4f1b69edaf667cc7
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2022-12-21 11:11:33 +0000

    Added compliance test with multiple parents

--- tests/unit/mock/config/compliance/compliance/extreme_netiron/netiron_basic_feature.py
+++ tests/unit/mock/config/compliance/compliance/extreme_netiron/netiron_basic_feature.py
@@ -1,4 +1,5 @@
 features = [
     {"name": "syslog", "ordered": False, "section": ["logging"]},
     {"name": "aaa", "ordered": False, "section": ["aaa"]},
+    {"name": "isis", "ordered": True, "section": ["router isis"]},
 ]
