4ed67d596137d0247447ea100a3ba644f260db84
Andrew Bates <andrew.bates@networktocode.com>
2023-01-05 16:03:24 -0500

    Fixed black formatting

--- tests/unit/test_ping.py
+++ tests/unit/test_ping.py
@@ -10,8 +10,13 @@ ping_data = [
     {"sent": {"ip": "1.1.1.1", "port": 443}, "received": {"retval": True}},
     {"sent": {"ip": "192.0.2.0", "port": 443}, "received": {"retval": False}, "raises": socket.timeout},
     {"sent": {"ip": "1.1.1.1", "port": 443, "timeout": 3}, "received": {"retval": True}},
-    {"sent": {"ip": "nevergonnagiveyouup.pizza", "port": 443}, "received": {"retval": None, "raised": socket.gaierror}, "raises": socket.gaierror},
+    {
+        "sent": {"ip": "nevergonnagiveyouup.pizza", "port": 443},
+        "received": {"retval": None, "raised": socket.gaierror},
+        "raises": socket.gaierror,
+    },
 ]
+
 
 @pytest.mark.parametrize("data", ping_data)
 def test_tcp_ping(data):
@@ -34,5 +39,3 @@ def test_tcp_ping(data):
         instance.connect.assert_called_with((ip, port))
 
         instance.close.assert_called()
-
-
