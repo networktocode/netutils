44c87b4aad5269dff66fd0e1aa1ef89eca9f1cf5
Adam Byczkowski <adam.byczkowski@kochgs.com>
2023-03-17 16:13:45 -0500

    Drop support for python 3.7

--- tasks.py
+++ tasks.py
@@ -31,7 +31,7 @@ PYPROJECT_CONFIG = toml.load("pyproject.toml")
 TOOL_CONFIG = PYPROJECT_CONFIG["tool"]["poetry"]
 
 # Can be set to a separate Python version to be used for launching or building image
-PYTHON_VER = os.getenv("PYTHON_VER", "3.9")
+PYTHON_VER = os.getenv("PYTHON_VER", "3.11")
 # Name of the docker image/image
 IMAGE_NAME = os.getenv("IMAGE_NAME", TOOL_CONFIG["name"])
 # Tag for the image
