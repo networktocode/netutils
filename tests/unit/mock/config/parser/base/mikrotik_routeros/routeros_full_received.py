14ccd4d8fc844b3b17eff0c4ff969926ac1c3825
h4ndzdatm0ld <hugotinoco@icloud.com>
2023-04-01 21:23:24 +0000

    feedback: Parsing tests for system note

--- tests/unit/mock/config/parser/base/mikrotik_routeros/routeros_full_received.py
+++ tests/unit/mock/config/parser/base/mikrotik_routeros/routeros_full_received.py
@@ -116,4 +116,36 @@ data = [
     ConfigLine(config_line="/system identity set name=ag1.123site.nwk.nj", parents=()),
     ConfigLine(config_line="/system logging add action=remote topics=error", parents=()),
     ConfigLine(config_line="/system logging add action=remote topics=info", parents=()),
+    ConfigLine(
+        config_line='/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',
+        parents=(),
+    ),
+    ConfigLine(
+        config_line='    \\nIt includes double quotes (\\\\\\") and special characters such as:\\\\n\\\\\\',
+        parents=('/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',),
+    ),
+    ConfigLine(
+        config_line="    \\n@, #, \\$, %, ^, &, *, (, ), _, +, [, ], {, }, |, ;, ',', ., /, <, >, and \\?.\\\\n\\\\\\",
+        parents=('/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',),
+    ),
+    ConfigLine(
+        config_line="    \\n\\\\n\\\\\\",
+        parents=('/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',),
+    ),
+    ConfigLine(
+        config_line="    \\nRemember to escape any special characters with a backslash (\\\\\\\\) when necessary.\\\\n\\\\\\",
+        parents=('/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',),
+    ),
+    ConfigLine(
+        config_line="    \\n\\\\n\\\\\\",
+        parents=('/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',),
+    ),
+    ConfigLine(
+        config_line="    \\nThis is a multiline note with several lines of text.\\\\n\\\\\\",
+        parents=('/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',),
+    ),
+    ConfigLine(
+        config_line='    \\nWow, what a great example of a note to ensure proper parsing by NetUtils!"',
+        parents=('/system note set note="This is a \\\\\\"System Note\\\\\\" for a Mikrotik router.\\\\n\\\\\\',),
+    ),
 ]
