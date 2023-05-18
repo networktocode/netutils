31001f92ca9aecb57ac37d4406e14f2812e01f82
bile0026 <zach.biles@networktocode.com>
2023-05-18 16:04:48 -0500

    fix: 🐛 update jinja mapping

--- netutils/utils.py
+++ netutils/utils.py
@@ -67,6 +67,7 @@ _JINJA2_FUNCTION_MAPPINGS = {
     "uptime_seconds_to_string": "time.uptime_seconds_to_string",
     "uptime_string_to_seconds": "time.uptime_string_to_seconds",
     "get_napalm_getters": "lib_helpers.get_napalm_getters",
+    "paloalto_panos_brace_to_set": "config.conversion.paloalto_panos_brace_to_set"
 }
 
 
