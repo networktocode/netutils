feed96fe55d13e3623910ecce0195d76e542ca06
bile0026 <zach.biles@networktocode.com>
2023-05-18 16:00:26 -0500

    test: ✅ add test features

---
+++ tests/unit/mock/config/compliance/section/paloalto_panos/panos_full_feature.py
@@ -0,0 +1,1 @@
+feature = {"name": "many", "ordered": True, "section": ["set mgt-config", "set devices localhost.localdomain deviceconfig system panorama"]}
--- tests/unit/mock/config/parser/base/paloalto_panos/panos_full_received.py
+++ tests/unit/mock/config/parser/base/paloalto_panos/panos_full_received.py
@@ -1,6 +1,7 @@
 from netutils.config.parser import ConfigLine
 
 data = [
+    ConfigLine(config_line="set mgt-config users admin phash *", parents=()),
     ConfigLine(config_line="set mgt-config users admin permissions role-based superuser yes", parents=()),
     ConfigLine(
         config_line="set mgt-config users admin public-key thisisasuperduperlongbase64encodedstring=", parents=()
@@ -279,22 +280,22 @@ data = [
         config_line="set devices localhost.localdomain deviceconfig system service disable-http yes", parents=()
     ),
     ConfigLine(config_line="set devices localhost.localdomain deviceconfig system hostname firewall1", parents=()),
-    ConfigLine(config_line='set system login-banner "', parents=()),
-    ConfigLine(config_line=" ************************************************************************", parents=()),
+    ConfigLine(config_line='set devices localhost.localdomain deviceconfig system login-banner "', parents=()),
+    ConfigLine(config_line=" ************************************************************************", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
     ConfigLine(
         config_line=" *                        firewall1.example.com                       *                         [PROD VM500  firewalls]",
-        parents=(),
+        parents=("set devices localhost.localdomain deviceconfig system login-banner \"",),
     ),
-    ConfigLine(config_line=" ************************************************************************", parents=()),
-    ConfigLine(config_line=" *                               WARNING                                *", parents=()),
-    ConfigLine(config_line=" *   Unauthorized access to this device or devices attached to          *", parents=()),
-    ConfigLine(config_line=" *   or accessible from this network is strictly prohibited.            *", parents=()),
-    ConfigLine(config_line=" *   Possession of passwords or devices enabling access to this         *", parents=()),
-    ConfigLine(config_line=" *   device or devices does not constitute authorization. Unauthorized  *", parents=()),
-    ConfigLine(config_line=" *   access will be prosecuted to the fullest extent of the law.        *", parents=()),
-    ConfigLine(config_line=" *                                                                      *", parents=()),
-    ConfigLine(config_line=" ************************************************************************", parents=()),
-    ConfigLine(config_line=' "', parents=()),
+    ConfigLine(config_line=" ************************************************************************", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" *                               WARNING                                *", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" *   Unauthorized access to this device or devices attached to          *", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" *   or accessible from this network is strictly prohibited.            *", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" *   Possession of passwords or devices enabling access to this         *", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" *   device or devices does not constitute authorization. Unauthorized  *", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" *   access will be prosecuted to the fullest extent of the law.        *", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" *                                                                      *", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=" ************************************************************************", parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
+    ConfigLine(config_line=' "', parents=("set devices localhost.localdomain deviceconfig system login-banner \"",)),
     ConfigLine(
         config_line="set devices localhost.localdomain deviceconfig system panorama local-panorama panorama-server 10.0.0.1",
         parents=(),
