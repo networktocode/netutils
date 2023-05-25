531a2a398c20c4503552e522f90a9bfd99d85d59
bile0026 <zach.biles@networktocode.com>
2023-05-25 14:36:04 -0500

    fix: 🐛 adjust banner handling

--- netutils/config/parser.py
+++ netutils/config/parser.py
@@ -1427,12 +1427,44 @@ class PaloAltoNetworksConfigParser(BaseSpaceConfigParser):
     """Palo Alto Networks config parser."""
 
     comment_chars: t.List[str] = []
-    banner_start: t.List[str] = ['set system login-banner "']
+    banner_start: t.List[str] = ['set system login-banner "', 'login-banner "']
 
     @property
     def banner_end(self) -> str:
         """Demarcate End of Banner char(s)."""
-        raise NotImplementedError("Palo Alto Networks platform doesn't have a banner.")
+        raise NotImplementedError("Palo Alto Networks platform doesn't have a end of banner character.")
+
+    def is_banner_end(self, line: str) -> bool:
+        """Determine if end of banner."""
+        if line.endswith('"') or line.startswith('";') or line.startswith("set"):
+            return True
+        return False
+
+    def _build_banner(self, config_line: str) -> t.Optional[str]:
+        """Handle banner config lines.
+
+        Args:
+            config_line: The start of the banner (system note) config.
+
+        Returns:
+            The next configuration line in the configuration text or None when banner end is the end of the config text.
+
+        Raises:
+            ValueError: When the parser is unable to identify the End of the Banner.
+        """
+        banner_config = [config_line]
+        for line in self.generator_config:
+            if not self.is_banner_end(line):
+                banner_config.append(line)
+            else:
+                banner_config.append(line)
+                line = "\n".join(banner_config)
+                self._update_config_lines(line)
+                try:
+                    return next(self.generator_config)
+                except StopIteration:
+                    return None
+        raise ValueError("Unable to parse banner end.")
 
     def build_config_relationship(self) -> t.List[ConfigLine]:
         r"""Parse text of config lines and find their parents.
