c8d9f2fcbd8d56c5fa94a140c215d6bc906344f8
Przemek Rogala <progala@progala.net>
2023-08-08 13:23:56 +0100

    Redo object factory using base class. Rename factory fun.

--- netutils/utils.py
+++ netutils/utils.py
@@ -89,6 +89,7 @@ _JINJA2_FUNCTION_MAPPINGS = {
     "paloalto_panos_brace_to_set": "config.conversion.paloalto_panos_brace_to_set",
     "get_upgrade_path": "os_version.get_upgrade_path",
     "create_platform_object": "platform_mapper.create_platform_object",
+    "os_platform_object_builder": "platform_mapper.os_platform_object_builder",
     "juniper_junos_version_parser": "os_version_parser.juniper_junos_version_parser",
 }
 
