08580d5312b06a9e874a5f7efdd7ff6dbd540e2e
gt732 <83186740+gt732@users.noreply.github.com>
2023-09-07 14:39:27 -0400

    Ubiquiti airFiber Parser (#329)
    * Revised Ubiquiti AirFiber parser
    
    * Changed parser name to device OS instead
    
    * Corrected parser docstrings to airOS
    
    * updated parser to remove # lines
    
    * Fixed black issue

--- netutils/config/compliance.py
+++ netutils/config/compliance.py
@@ -26,6 +26,7 @@ parser_map: t.Dict[str, t.Type[parser.BaseConfigParser]] = {
     "paloalto_panos": parser.PaloAltoNetworksConfigParser,
     "mikrotik_routeros": parser.RouterOSConfigParser,
     "ruckus_fastiron": parser.FastironConfigParser,
+    "ubiquiti_airos": parser.UbiquitiAirOSConfigParser,
 }
 
 
---
+++ tests/unit/mock/config/compliance/compliance/ubiquiti_airos/airos_feature.py
@@ -0,0 +1,5 @@
+features = [
+    {"name": "radio_country_code", "ordered": True, "section": ["radio.1.countrycode"]},
+    {"name": "snmp", "ordered": True, "section": ["snmp.community"]},
+    {"name": "radio_mode", "ordered": True, "section": ["radio.1.mode"]},
+]
