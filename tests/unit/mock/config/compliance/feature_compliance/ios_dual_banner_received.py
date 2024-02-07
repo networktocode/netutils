27122a33a284b5ea8c6495302fd840c97b08a2b1
Anthony House <anthony.house@networktocode.com>
2024-02-07 09:21:09 -0500

    Refactor test, implement dual banner fix

--- tests/unit/mock/config/compliance/feature_compliance/ios_dual_banner_received.py
+++ tests/unit/mock/config/compliance/feature_compliance/ios_dual_banner_received.py
@@ -1,10 +1,10 @@
 data = {
     "compliant": False,
-    "missing": "banner exec ^C\n=========\nintended config exec banner\n-========^C\n",
-    "extra": "banner exec ^C\n=========\nactual config exec banner\n-========^C\n",
+    "missing": "banner exec ^C\n=========\nintended config exec banner\n-========^C",
+    "extra": "banner exec ^C\n=========\nactual config exec banner\n-========^C",
     "cannot_parse": True,
     "unordered_compliant": False,
     "ordered_compliant": False,
-    "actual": "hostname dual-banner\n!\nbanner exec ^C\n=========\nactual config exec banner\n-========\n^C\nbanner motd ^C\n======\nactual config motd banner\n======\n   || ($hostname) ||\n^C\n!\n",
-    "intended": "hostname dual-banner\n!\nbanner exec ^C\n=========\nintended config exec banner\n-========\n^C\nbanner motd ^C\n======\nintended config motd banner\n======\n   || ($hostname) ||\n^C\n!",
+    "actual": "banner exec ^C\n=========\nactual config exec banner\n-========^C",
+    "intended": "banner exec ^C\n=========\nintended config exec banner\n-========^C",
 }
