75d8801315bf3ba375264cfec3b4236a6ca19c1a
Anthony House <anthony.house@networktocode.com>
2024-02-07 09:57:24 -0500

    Add tests for multiple banners

---
+++ tests/unit/mock/config/compliance/feature_compliance/ios_dual_banner_motd_received.py
@@ -0,0 +1,10 @@
+data = {
+    "compliant": False,
+    "missing": "banner motd ^C\n======\nintended config motd banner\n======\n   || ($hostname) ||^C",
+    "extra": "banner motd ^C\n======\nactual config motd banner\n======\n   || ($hostname) ||^C",
+    "cannot_parse": True,
+    "unordered_compliant": False,
+    "ordered_compliant": False,
+    "actual": "banner motd ^C\n======\nactual config motd banner\n======\n   || ($hostname) ||^C",
+    "intended": "banner motd ^C\n======\nintended config motd banner\n======\n   || ($hostname) ||^C",
+}
--- tests/unit/mock/config/compliance/feature_compliance/ios_dual_banner_received.py
+++ tests/unit/mock/config/compliance/feature_compliance/ios_dual_banner_received.py
@@ -1,10 +1,10 @@
 data = {
     "compliant": False,
-    "missing": "banner exec ^C\n=========\nintended config exec banner\n-========^C",
-    "extra": "banner exec ^C\n=========\nactual config exec banner\n-========^C",
+    "missing": "banner exec ^C\n=========\nintended config exec banner\n-========^C\nbanner motd ^C\n======\nintended config motd banner\n======\n   || ($hostname) ||^C",
+    "extra": "banner exec ^C\n=========\nactual config exec banner\n-========^C\nbanner motd ^C\n======\nactual config motd banner\n======\n   || ($hostname) ||^C",
     "cannot_parse": True,
     "unordered_compliant": False,
     "ordered_compliant": False,
-    "actual": "banner exec ^C\n=========\nactual config exec banner\n-========^C",
-    "intended": "banner exec ^C\n=========\nintended config exec banner\n-========^C",
+    "actual": "banner exec ^C\n=========\nactual config exec banner\n-========^C\nbanner motd ^C\n======\nactual config motd banner\n======\n   || ($hostname) ||^C",
+    "intended": "banner exec ^C\n=========\nintended config exec banner\n-========^C\nbanner motd ^C\n======\nintended config motd banner\n======\n   || ($hostname) ||^C",
 }
