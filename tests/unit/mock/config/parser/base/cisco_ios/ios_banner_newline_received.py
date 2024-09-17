b33408ea56a5979ed8d5a747b3cf356c938c8053
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2024-09-17 16:08:20 -0600

    failing test for banner stripping trailing newlines during parsing (#569)
    * failing test for banner stripping trailing newlines during parsing
    
    * possible fix to rstrip of banners
    
    * fix all banner test
    
    ---------
    
    Co-authored-by: Jeff Kala <jeff.kala@chevron.com>

---
+++ tests/unit/mock/config/parser/base/cisco_ios/ios_banner_newline_received.py
@@ -0,0 +1,12 @@
+from netutils.config.parser import ConfigLine
+
+data = [
+    ConfigLine(config_line="hostname banner_with_newline", parents=()),
+    ConfigLine(config_line="banner login ^C", parents=()),
+    ConfigLine(
+        config_line="****************************************************\nWARNING TO UNAUTHORIZED USERS:\nThis system is for use by authorized users only.\n****************************************************\n^C",
+        parents=("banner login ^C",),
+    ),
+    ConfigLine(config_line="line vty 0 4", parents=()),
+    ConfigLine(config_line=" transport ssh", parents=("line vty 0 4",)),
+]
