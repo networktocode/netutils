6aef4f7a957c5bd8056cdaf8454f88de398cea6e
Ryan Gillespie <24619595+nopg@users.noreply.github.com>
2025-05-07 08:52:34 -0600

    Fix Palo set format config parsing if curly brace exists (#651)
    * allow curly braces in palo set format
    
    * switch to splitlines

---
+++ tests/unit/mock/config/parser/base/paloalto_panos/panos_basic_set_received.py
@@ -0,0 +1,48 @@
+from netutils.config.parser import ConfigLine
+
+data = [
+    ConfigLine(
+        config_line="set mgt-config users admin phash *",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line="set mgt-config users admin permissions role-based superuser yes",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line="set mgt-config users admin public-key thisisasuperduperlongbase64encodedstring",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line="set mgt-config users panadmin permissions role-based superuser yes",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line="set mgt-config users panadmin phash passwordhash",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line="set deviceconfig system hostname firewall1",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line='set deviceconfig system login-banner "',
+        parents=(),
+    ),
+    ConfigLine(
+        config_line="************************************************************************\n*                        firewall1.example.com                       *                         [PROD VM500  firewalls]\n************************************************************************\n*                               WARNING                                *\n*   Unauthorized access to this device or devices attached to          *\n*   or accessible from this network is strictly prohibited.            *\n*   Possession of passwords or devices enabling access to this         *\n*   device or devices does not constitute authorization. Unauthorized  *\n*   access will be prosecuted to the fullest extent of the law.        *\n*                                                                      *\n************************************************************************^C",
+        parents=('set deviceconfig system login-banner "',),
+    ),
+    ConfigLine(
+        config_line="set deviceconfig system panorama local-panorama panorama-server 10.0.0.1",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line="set deviceconfig system panorama local-panorama panorama-server-2 10.0.0.2",
+        parents=(),
+    ),
+    ConfigLine(
+        config_line='set network interface ethernet ethernet1/15 comment "test curly braces {REMOTE DEVICE}, {REMOTE PORT}"',
+        parents=(),
+    ),
+]
