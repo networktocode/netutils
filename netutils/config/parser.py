d7230f55fdb7691051087ac1919bdd1b77e9aaf4
julmanglano <60711820+julmanglano@users.noreply.github.com>
2025-08-01 01:53:01 +0200

    Leading spaces (#657)
    * Catching leading spaces at config start
    
    * spacing tests
    
    * after black
    
    * remoing wrong variable assigment
    
    * adding parametrize
    
    * reforming
    
    * black formatting
    
    * Update tests/unit/test_parser.py
    
    Co-authored-by: Ken Celenza <ken@celenza.org>
    
    * disable=redefined-outer-name
    
    * fromatting...
    
    ---------
    
    Co-authored-by: Ken Celenza <ken@celenza.org>

--- netutils/config/parser.py
+++ netutils/config/parser.py
@@ -312,8 +312,11 @@ class BaseSpaceConfigParser(BaseConfigParser):
             ... ]
             True
         """
-        for line in self.generator_config:
-            if not line[0].isspace():
+        for index, line in enumerate(self.generator_config):
+            current_spaces = self.get_leading_space_count(line) if line[0].isspace() else 0
+            if index == 0 and line[0].isspace():
+                self._current_parents = self._remove_parents(line, current_spaces)
+            elif not line[0].isspace():
                 self._current_parents = ()
                 if self.is_banner_start(line):
                     line = self._build_banner(line)  # type: ignore
@@ -988,8 +991,11 @@ class ASAConfigParser(CiscoConfigParser):
             ... ]
             True
         """
-        for line in self.generator_config:
-            if not line[0].isspace():
+        for index, line in enumerate(self.generator_config):
+            current_spaces = self.get_leading_space_count(line) if line[0].isspace() else 0
+            if index == 0 and line[0].isspace():
+                self._current_parents = self._remove_parents(line, current_spaces)
+            elif not line[0].isspace():
                 self._current_parents = ()
             else:
                 previous_config = self.config_lines[-1]
@@ -1386,7 +1392,11 @@ class IOSXRConfigParser(CiscoConfigParser):
             ... ]
             True
         """
-        for line in self.generator_config:
+        for index, line in enumerate(self.generator_config):
+            current_spaces = self.get_leading_space_count(line) if line[0].isspace() else 0
+
+            if index == 0 and line[0].isspace():
+                self._current_parents = self._remove_parents(line, current_spaces)
             if not line[0].isspace():
                 self._current_parents = ()
                 if self.is_banner_start(line):
