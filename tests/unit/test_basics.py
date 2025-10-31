ab2908f8206c322002a01dc3b0ad7083326dd130
Jeff Kala <Jeff.kala@networktocode.com>
2025-10-31 15:12:16 -0600

    remainder of the drift fixes

--- tests/unit/test_basics.py
+++ tests/unit/test_basics.py
@@ -7,25 +7,6 @@ import unittest
 import toml
 
 
-class TestDocsPackaging(unittest.TestCase):
-    """Test Version in doc requirements is the same pyproject."""
-
-    def test_version(self):
-        """Verify that pyproject.toml dev dependencies have the same versions as in the docs requirements.txt."""
-        parent_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-        poetry_path = os.path.join(parent_path, "pyproject.toml")
-        poetry_details = toml.load(poetry_path)["tool"]["poetry"]["group"]["dev"]["dependencies"]
-        with open(f"{parent_path}/docs/requirements.txt", "r", encoding="utf-8") as file:
-            requirements = [line for line in file.read().splitlines() if (len(line) > 0 and not line.startswith("#"))]
-        for pkg in requirements:
-            package_name = pkg
-            if len(pkg.split("==")) == 2:  # noqa: PLR2004
-                package_name, version = pkg.split("==")
-            else:
-                version = "*"
-            self.assertEqual(poetry_details[package_name], version)
-
-
 class TestDocsReleaseNotes(unittest.TestCase):
     """Test that mkdocs has the release notes for the current version."""
 
