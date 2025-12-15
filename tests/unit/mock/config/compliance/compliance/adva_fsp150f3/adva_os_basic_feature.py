3e2a1da1a77c97705bb636d724cb1d303e0929c7
Maximilian Francis <max.francis@networktocode.com>
2025-12-15 17:02:19 -0600

    Added compliance testing for adva_fsp150f3 driver.

---
+++ tests/unit/mock/config/compliance/compliance/adva_fsp150f3/adva_os_basic_feature.py
@@ -0,0 +1,5 @@
+features = [
+    {"name": "system", "ordered": False, "section": ["configure system"]},
+    {"name": "snmp", "ordered": False, "section": ["configure snmp"]},
+    {"name": "access port", "ordered": False, "section": ["    configure access-port"]}
+]
