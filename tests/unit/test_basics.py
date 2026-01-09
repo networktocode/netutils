9a57ea595c62d8c43ed98682e025d0fd32922c06
Jeff Kala <Jeff.kala@networktocode.com>
2026-01-09 13:41:39 -0700

    clean up merge conflicts for drift mgr

--- tests/unit/test_basics.py
+++ tests/unit/test_basics.py
@@ -12,18 +12,12 @@ class TestDocsReleaseNotes(unittest.TestCase):
 
     def test_version_file_found(self):
         """Verify that if the current version has no letters, which would see in alpha or beta has an associated release note file."""
-        parent_path = os.path.dirname(
-            os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-        )
+        parent_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
         poetry_path = os.path.join(parent_path, "pyproject.toml")
         project_version = toml.load(poetry_path)["tool"]["poetry"]["version"]
 
         docs_path = os.path.join(parent_path, "docs")
-        release_notes_files = [
-            file
-            for file in os.listdir(f"{docs_path}/admin/release_notes/")
-            if file.endswith(".md")
-        ]
+        release_notes_files = [file for file in os.listdir(f"{docs_path}/admin/release_notes/") if file.endswith(".md")]
         version_pattern = re.compile(r"^(\d+)\.(\d+)\.\d+$")
 
         match = version_pattern.match(project_version)
@@ -32,6 +26,4 @@ class TestDocsReleaseNotes(unittest.TestCase):
             major, minor = match.groups()
             version_str = f"version_{major}.{minor}.md"
             if version_str not in release_notes_files:
-                self.fail(
-                    f"Release note file for version {version_str} not found in release notes folder."
-                )
+                self.fail(f"Release note file for version {version_str} not found in release notes folder.")
