dcd6f0e47e05ef7d2a11fda4df5f9fc62ed73dd0
Jeff Kala <Jeff.kala@networktocode.com>
2026-01-09 13:35:37 -0700

    clean up merge conflicts for drift mgr

--- tests/unit/test_basics.py
+++ tests/unit/test_basics.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-"""Basic tests that do not require netutils."""
-=======
 """Basic tests that do not require Netutils."""
->>>>>>> 1476cba (Cookie updated targeting develop by NetworkToCode Cookie Drift Manager Tool)
-=======
-"""Basic tests that do not require Netutils."""
->>>>>>> 35e1395 (Cookie updated targeting develop by NetworkToCode Cookie Drift Manager Tool)
 
 import os
 import re
@@ -20,12 +12,18 @@ class TestDocsReleaseNotes(unittest.TestCase):
 
     def test_version_file_found(self):
         """Verify that if the current version has no letters, which would see in alpha or beta has an associated release note file."""
-        parent_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+        parent_path = os.path.dirname(
+            os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        )
         poetry_path = os.path.join(parent_path, "pyproject.toml")
         project_version = toml.load(poetry_path)["tool"]["poetry"]["version"]
 
         docs_path = os.path.join(parent_path, "docs")
-        release_notes_files = [file for file in os.listdir(f"{docs_path}/admin/release_notes/") if file.endswith(".md")]
+        release_notes_files = [
+            file
+            for file in os.listdir(f"{docs_path}/admin/release_notes/")
+            if file.endswith(".md")
+        ]
         version_pattern = re.compile(r"^(\d+)\.(\d+)\.\d+$")
 
         match = version_pattern.match(project_version)
@@ -34,4 +32,6 @@ class TestDocsReleaseNotes(unittest.TestCase):
             major, minor = match.groups()
             version_str = f"version_{major}.{minor}.md"
             if version_str not in release_notes_files:
-                self.fail(f"Release note file for version {version_str} not found in release notes folder.")
+                self.fail(
+                    f"Release note file for version {version_str} not found in release notes folder."
+                )
