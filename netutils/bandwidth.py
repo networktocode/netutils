f741083fdd18be89dce5bc4631b5f4e1e3929046
Andrea Dainese <andrea.dainese@pm.me>
2026-04-23 13:06:06 +0200

    Implements Mb/s

--- netutils/bandwidth.py
+++ netutils/bandwidth.py
@@ -38,6 +38,7 @@ BYTES_MAPPING = _get_bytes_mapping()
 
 def _normalize_bw(speed: str) -> str:
     speed = speed.replace("bit", "b")
+    speed = speed.replace("b/s", "b")
     per_second_mapping = {
         "b": "bps",
         "Kb": "Kbps",
